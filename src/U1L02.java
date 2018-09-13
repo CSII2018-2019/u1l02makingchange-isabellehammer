6f694b027adebcb924ca789477f3087e6f0dd6fa
Isabelle <hammeri@theellisschool.org>
2018-09-13 09:18:43 -0400

    code for calculating nickels and pennies using a loop

--- src/U1L02.java
+++ src/U1L02.java
@@ -1,8 +1,32 @@
+import javax.swing.JOptionPane;
 
 public class U1L02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//define variables for amounts of each coin
+		int nickels = 0;
+		int pennies = 0;
+		
+		//get input from user (change to usable value)
+		String answer = JOptionPane.showInputDialog("Enter a monetary value (ex: 1.56): ");
+		double cash = Double.parseDouble(answer);
+		pennies = (int)(cash * 100);
+		//System.out.println(pennies);
+	
+
+		
+		//loop subtracting coin amount until money < coin amount
+		while (pennies > 5) {
+			nickels++;
+			pennies -= 5;
+	
+		}
+			
+		
+		//pennies = money * 100 <-- don't need b/c converted to integer value
+		
+		//print answer
+		JOptionPane.showMessageDialog(null, cash + " is " + nickels + " nickels " + pennies + " pennies.");
 
 	}
 
