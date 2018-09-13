fc67192036d137806dd4c4c368e072de40b628b3
Isabelle <hammeri@theellisschool.org>
2018-09-13 09:31:38 -0400

    mathematical method

--- src/U1L02.java
+++ src/U1L02.java
@@ -4,13 +4,13 @@ public class U1L02 {
 
 	public static void main(String[] args) {
 		//define variables for amounts of each coin
-		int nickels = 0;
 		int pennies = 0;
 		
 		//get input from user (change to usable value)
 		String answer = JOptionPane.showInputDialog("Enter a monetary value (ex: 1.56): ");
 		double cash = Double.parseDouble(answer);
 		pennies = (int)(cash * 100);
+		 int nickels = 0;
 		//System.out.println(pennies);
 	
 
@@ -19,6 +19,15 @@ public class U1L02 {
 		while (pennies > 5) {
 			nickels++;
 			pennies -= 5;
+			
+			
+		// MATHEMATICAL METHOD
+		// GET INPUT FROM USER
+		// divide by 5 and round down
+		int money = (int)(cash/0.05);
+		System.out.println(nickels);
+		// subtract # of nickels times 5 from amount to get pennies
+		// print answer
 	
 		}
 			
@@ -27,6 +36,7 @@ public class U1L02 {
 		
 		//print answer
 		JOptionPane.showMessageDialog(null, cash + " is " + nickels + " nickels " + pennies + " pennies.");
+		
 
 	}
 
