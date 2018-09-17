c59132f72e8d02e632c9d1a10a5578f44730151f
Isabelle <hammeri@theellisschool.org>
2018-09-17 11:18:32 -0400

    this is not working so I'm pretty sure it is not correct

--- src/U1L02.java
+++ src/U1L02.java
@@ -11,12 +11,18 @@ public class U1L02 {
 		double cash = Double.parseDouble(answer);
 		pennies = (int)(cash * 100);
 		 int nickels = 0;
+		 int dimes = 0;
+		 int quarters = 0;
+		dimes = (int)(cash * 10);
+		quarters = (int)(cash * 4);
 		//System.out.println(pennies);
 	
 
 		
 		//loop subtracting coin amount until money < coin amount
 		while (pennies > 5) {
+			quarters++;
+			dimes++;
 			nickels++;
 			pennies -= 5;
 			
@@ -24,8 +30,9 @@ public class U1L02 {
 		// MATHEMATICAL METHOD
 		// GET INPUT FROM USER
 		// divide by 5 and round down
-		int money = (int)(cash/0.05);
 		System.out.println(nickels);
+		System.out.println(dimes);
+		System.out.println(quarters);
 		// subtract # of nickels times 5 from amount to get pennies
 		// print answer
 	
@@ -35,7 +42,7 @@ public class U1L02 {
 		//pennies = money * 100 <-- don't need b/c converted to integer value
 		
 		//print answer
-		JOptionPane.showMessageDialog(null, cash + " is " + nickels + " nickels " + pennies + " pennies.");
+		JOptionPane.showMessageDialog(null, cash + " is " + nickels + " nickels " + pennies + " pennies " + dimes + " dimes " + "and " + quarters + " quarters.");
 		
 
 	}
