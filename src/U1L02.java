0204406cce292428de666b32376f588f296fb35a
Isabelle <hammeri@theellisschool.org>
2018-09-23 11:15:21 -0400

    The code is able to run and I tried several combinations but I could not figure out what mathematically I was doing wrong. I will come back to it later but I have other homework and college commitments I need to address.

--- src/U1L02.java
+++ src/U1L02.java
@@ -4,27 +4,37 @@ public class U1L02 {
 
 	public static void main(String[] args) {
 		//define variables for amounts of each coin
-		int pennies = 0;
+		/*
+		 * ok so I just put these in comments because I tried to figure out how to solve
+		 * this but it won't work so now I;m just going to try with modular.
+		 * 
+		int pennies = 1;
 		
 		//get input from user (change to usable value)
 		String answer = JOptionPane.showInputDialog("Enter a monetary value (ex: 1.56): ");
 		double cash = Double.parseDouble(answer);
 		pennies = (int)(cash * 100);
-		 int nickels = 0;
-		 int dimes = 0;
-		 int quarters = 0;
-		dimes = (int)(cash * 10);
-		quarters = (int)(cash * 4);
+		 int nickels = 5;
+		 int dimes = 10;
+		 int quarters = 25;
 		//System.out.println(pennies);
 	
 
 		
 		//loop subtracting coin amount until money < coin amount
 		while (pennies > 5) {
-			quarters++;
-			dimes++;
-			nickels++;
 			pennies -= 5;
+			
+		}
+		while (nickels > 5) {
+			nickels -= 5;
+		}
+		while (dimes > 10) {
+			dimes -= 10;
+		}
+		while (quarters > 1) {
+			quarters -= 25;
+		}
 			
 			
 		// MATHEMATICAL METHOD
@@ -36,13 +46,39 @@ public class U1L02 {
 		// subtract # of nickels times 5 from amount to get pennies
 		// print answer
 	
-		}
 			
 		
 		//pennies = money * 100 <-- don't need b/c converted to integer value
 		
 		//print answer
 		JOptionPane.showMessageDialog(null, cash + " is " + nickels + " nickels " + pennies + " pennies " + dimes + " dimes " + "and " + quarters + " quarters.");
+		*/
+		
+		
+		
+		String answer = JOptionPane.showInputDialog("Enter a monetary value (ex: 1.56): ");
+		double cash = Double.parseDouble(answer);
+		//pennies = (int)(cash * 100);
+		//nickels = (int)(cash * 20);
+		//dimes = (int)(cash * 10);
+		//quarters = (int)(cash * 4);
+		
+		double pennies = 1;
+		double nickels = 5;
+		double dimes = 10;
+		double quarters = 25;
+		
+		double totalquarters = (cash / quarters);
+		cash %= quarters;
+		double totalnickels = (cash / nickels);
+		cash %= nickels;
+		double totaldimes = (cash / dimes);
+		cash %= dimes;
+		double totalpennies = (cash / pennies);
+		cash %= pennies;
+		
+		JOptionPane.showMessageDialog(null, cash + " is " + totalnickels + " nickels " + totalpennies + 
+				" pennies " + totaldimes + " dimes " + "and " + totalquarters + " quarters.");
 		
 
 	}
